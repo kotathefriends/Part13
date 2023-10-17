ba963d0f2ea2dc1409c454665ea04919669a139b
kotathefriends <5070kouta@gmail.com>
2023-10-17 14:43:03 +0900

    Initial Commit

---
+++ Part13/Part13App.swift
@@ -0,0 +1,17 @@
+//
+//  Part13App.swift
+//  Part13
+//
+//  Created by KOTA TAKAHASHI on 2023/10/17.
+//
+
+import SwiftUI
+
+@main
+struct Part13App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
